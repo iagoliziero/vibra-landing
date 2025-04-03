ccd6a1bdbdb80e93cb4bb68f745c6e0874cf519f
iagoliziero <iagolizierox14@gmail.com>
2025-04-03 13:08:36 -0300

    style: adjust dimensons (text, max-w)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { Menu } from "lucide-react";
 const Header = () => {
     return ( 
         <header className='bg-lightPink  fixed top-0 right-0 w-full  m-0 p-5 flex justify-center items-center  '>
-        <div className='flex justify-between items-center w-full max-w-4xl'>
-          <h1 className='font-darker text-3xl font-semibold lg:text-4xl '>Comunidade Vibra</h1>
+        <div className='flex justify-between items-center w-full max-w-4xl md:max-w-2xl'>
+          <h1 className='font-darker font-semibold text-3xl md:text-4xl xxl:text-[3rem] '>Comunidade Vibra</h1>
           <span> <Menu size={30} className="cursor-pointer" /> </span>
         </div>
       </header>
