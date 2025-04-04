70fe8abf0b38ea589e3cbad6477490c35173f692
iagoliziero <iagolizierox14@gmail.com>
2025-04-04 14:41:29 -0300

    feat: insert imgs to projet

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,15 +5,14 @@ import Paragraph from "./Paragraph";
 
 const Profile = () => {
     return ( 
-        <div className="flex flex-col items-center h-[47rem] lg:h-[37rem] w-full bg-lightPink overflow-hidden mb-10 *:lg:mb-0 lg:rounded-3xl shadow-lg shadow-lightPink/50">
-            <div className=' flex gap-8 max-w-[90%] '>
+        <div className="flex flex-col items-center h-[55rem] lg:h-[37rem] w-full bg-lightPink overflow-hidden mb-10 *:lg:mb-0 lg:rounded-3xl shadow-lg shadow-lightPink/50">
+            <div className=' flex gap-8 max-w-[80%] '>
                 <div className='text-center relative flex flex-col items-center justify-center  '>
                     <div className="mt-6 xxl:mt8 flex flex-col items-center justify-center max-w-4xl md:max-w-2xl">
                         <H1> 
                         Quem está por trás?
                         </H1>
-                    <div className="h-40 rounded-full w-40 bg-whiteBG mt-6  xxl:mt8 lg:hidden">
-                    </div>
+                    <img src="./public/imgAna.png" alt="Imagem Ana Luyza" className="h-60 rounded-full w-60 bg-whiteBG mt-6  xxl:mt8 lg:hidden" />
                     </div>
                     <span className="mt-6"> <Paragraph> 
                     Oi! Eu sou a Ana Luyza, influenciadora digital,
@@ -31,11 +30,10 @@ const Profile = () => {
                     </div>
                      </div>  
                      <div className="hidden lg:block relative top-20">
-                        <div className="h-60 rounded-full w-60 bg-whiteBG ">
-                    </div>
+                        <img  src="./public/imgAna.png" alt="Imagem Ana Luyza" className="h-[20rem] rounded-full w-[25rem] bg-whiteBG " />
                      </div>
           </div>
-          
+            
           
         </div>
      );
