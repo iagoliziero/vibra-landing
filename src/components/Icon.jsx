54b445b4afe12b1127ef47ca40a443e5efba8b43
iagoliziero <iagolizierox14@gmail.com>
2025-04-04 15:06:49 -0300

    feat: create Icon component

---
+++ src/components/Icon.jsx
@@ -0,0 +1,13 @@
+import Paragraph from "./Paragraph";
+
+const Icon = (props) => {
+    return ( 
+          <div className="text-center relative flex flex-col  gap-3 justify-center items-center ">
+            {props.children}
+          </div>
+          
+
+     );
+}
+ 
+export default Icon;
