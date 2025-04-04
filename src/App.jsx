53c2aff11752e734c4b38dbeb54346504af2741b
iagoliziero <iagolizierox14@gmail.com>
2025-04-04 09:48:00 -0300

    feat: insert SectionInformation and CardInformation to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import './App.css'
 import H1 from './components/H1'
 import Header from './components/Header'
 import Paragraph from './components/Paragraph'
-import { Brain, HandHeart, Leaf, Wind } from 'lucide-react'
-import SectionInformation from './components/SectionInformation'
+import CardInformation from './components/CardInformation'
+
 import Button from './components/Button'
+import SectionInformation from './components/SectionInformation'
 
 function App() {
 
 
   return (
     <>
-      <div className=' flex flex-col items-center  h-screen  bg-whiteMain m-0 p-0'>
+      <div className=' flex flex-col items-center  h-screen  bg-whiteMain m-0 p-0 overflow-auto'>
         <Header />
           <div className='mt-[3.5rem] xl:mt-16 flex flex-col gap-4 max-w-[90%]'>
             <div className='text-center relative'>
@@ -28,6 +29,22 @@ function App() {
           </div>
           <div className='mt-6 xxl:mt8'>
             <Button />
+          </div>
+          <div className=' flex flex-col gap-4 max-w-[90%]'>
+            <div className='text-center relative'>
+              <H1> 
+              O que é a vibra?
+              </H1>
+            </div>
+            <span> <Paragraph> 
+            É um grupo no WhatsApp só com meninas que querem sair da estagnação e começar a virar a chave.
+              </Paragraph> </span>
+              <div className='flex justify-center  xxl:mt8'>
+                <SectionInformation />
+              </div>
+              <div className='flex justify-center mt-6 xxl:mt8'>
+                <CardInformation />
+              </div>
           </div>
             
       </div>
