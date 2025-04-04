3d7c0b5f205b4392514b0e67ecc82d373aa54431
iagoliziero <iagolizierox14@gmail.com>
2025-04-04 16:34:43 -0300

    chore: install shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,80 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    screens: {
-      'md': '640px',
-      'mil': '960px',
-      'lg': '1024px',
-      'xl': '1281px',
-      'xxl': '1470px',
-       '3xl': '1820px',
-    },
-    extend: {
-      colors: {
-        whiteBG: '#F4F4F4',
-        lightPink: '#FFC0D6',
-        darkPink: '#E5457D',
-        black: '#1E1E1E',
-      },
-      fontFamily: {
-        sans: ['Darker Grotesque', 'sans-serif'], // replace with your custom font
-        darker: ['Darker Grotesque', 'sans-serif'], // custom font
-      },
-    },
+  	screens: {
+  		md: '640px',
+  		mil: '960px',
+  		lg: '1024px',
+  		xl: '1281px',
+  		xxl: '1470px',
+  		'3xl': '1820px'
+  	},
+  	extend: {
+  		colors: {
+  			whiteBG: '#F4F4F4',
+  			lightPink: '#FFC0D6',
+  			darkPink: '#E5457D',
+  			black: '#1E1E1E',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			sans: [
+  				'Darker Grotesque',
+  				'sans-serif'
+  			],
+  			darker: [
+  				'Darker Grotesque',
+  				'sans-serif'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
