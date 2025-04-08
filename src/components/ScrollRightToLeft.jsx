1068dde86c5e6fbb89c04a48eab6e141986ab9d5
iagoliziero <iagolizierox14@gmail.com>
2025-04-08 18:44:20 -0300

    feat: create ScrollLeftToRight using gsap

---
+++ src/components/ScrollRightToLeft.jsx
@@ -0,0 +1,37 @@
+// components/ScrollGsap.jsx
+import {useLayoutEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function ScrollRightToLeft({ children }) {
+  const sectionRef = useRef(null);
+
+  useLayoutEffect(() => {
+    const el = sectionRef.current;
+
+    gsap.fromTo(
+      el,
+      { opacity: 0, x: -100 },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1,
+        ease: 'power2.out',
+        scrollTrigger: {
+          trigger: el,
+          start: 'top 80%',
+          toggleActions: 'play none none reverse',
+        },
+      }
+    );
+
+  }, []);
+
+  return (
+    <div ref={sectionRef} className="w-full">
+      {children}
+    </div>
+  );
+}
